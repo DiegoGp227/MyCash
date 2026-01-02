d607e87f661ddfab02fb4ab643ed1813385e2f8f
DiegoGp227 <diego.gp227@gmail.com>
2026-01-01 21:21:15 -0500

    [ feat ] show password integrated

--- front/src/auth/form/LoginForm.tsx
+++ front/src/auth/form/LoginForm.tsx
@@ -3,11 +3,13 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { loginSchema, LoginInput } from "../shemas/auth.shemas";
-import { SendHorizontal } from "lucide-react";
+import { Eye, EyeClosed, SendHorizontal } from "lucide-react";
 import { useLogin } from "../hooks/useLogin";
+import { useState } from "react";
 
 export default function LoginForm() {
   const { error: apiError, isLoading, login } = useLogin();
+  const [showPassword, setShowPassword] = useState(false);
 
   const {
     register,
@@ -30,11 +32,12 @@ export default function LoginForm() {
       onSubmit={handleSubmit(onSubmit)}
       className="w-full flex flex-col gap-2"
     >
+      {/* EMAIL */}
       <label htmlFor="email" className="text-hard-gray">
         Email:*
       </label>
       <input
-        type="text"
+        type="email"
         id="email"
         {...register("email")}
         className="input text-hard-gray"
@@ -44,26 +47,46 @@ export default function LoginForm() {
         <p className="text-red-500 text-sm">{errors.email.message}</p>
       )}
 
+      {/* PASSWORD */}
       <label htmlFor="password" className="text-hard-gray">
         Password:*
       </label>
-      <input
-        type="password"
-        id="password"
-        {...register("password")}
-        className="input text-hard-gray"
-        disabled={isLoading}
-      />
+
+      <div className="relative">
+        <input
+          type={showPassword ? "text" : "password"}
+          id="password"
+          {...register("password")}
+          className="input text-hard-gray pr-10 w-full"
+          disabled={isLoading}
+        />
+
+        <button
+          type="button"
+          onClick={() => setShowPassword((prev) => !prev)}
+          className="absolute right-2 top-1/2 -translate-y-1/2 flex items-center justify-center p-1 hover:bg-primary-purple/10 rounded transition-colors"
+          aria-label={showPassword ? "Hide password" : "Show password"}
+        >
+          {showPassword ? (
+            <Eye color="#9333ea" size={20} />
+          ) : (
+            <EyeClosed color="#9333ea" size={20} />
+          )}
+        </button>
+      </div>
+
       {errors.password && (
         <p className="text-red-500 text-sm">{errors.password.message}</p>
       )}
 
+      {/* API ERROR */}
       {apiError && (
         <div className="bg-red-500/10 border border-red-500 text-red-500 px-4 py-2 rounded-md text-sm">
           {apiError}
         </div>
       )}
 
+      {/* SUBMIT */}
       <button
         type="submit"
         className="flex border-2 border-primary-purple hover:border-primary-purple-hover justify-center items-center py-2 px-2.5 rounded mt-2 hover:bg-main-purple gap-1"
