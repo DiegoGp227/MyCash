13b71bedb2f986d25e3794f70f5befa64260e9e0
DiegoGp227 <diego.gp227@gmail.com>
2026-01-01 20:51:59 -0500

    [ init+ ] hook login integrated

---
+++ front/src/auth/hooks/useLogin.ts
@@ -0,0 +1,44 @@
+import { useAppStoreActions } from "@/store/hooks";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+import { ICredentials } from "../types/auth.types";
+import { parseAuthError } from "@/utils/parseAuthError";
+
+export function useLogin() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const { setAuth } = useAppStoreActions((actions) => actions.auth);
+  const router = useRouter();
+
+  const login = useCallback(
+    async (credentials: ICredentials) => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await authService.login(credentials);
+
+        if (response?.token) {
+          localStorage.setItem("token", response.token);
+          localStorage.setItem("userInfo", JSON.stringify(response.userInfo));
+          setAuth(response);
+          router.push("/profile");
+        }
+
+        return response;
+      } catch (err: unknown) {
+        const errorMessage = parseAuthError(err);
+        setError(errorMessage);
+        throw new Error(errorMessage);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [setAuth, router]
+  );
+
+  return {
+    isLoading,
+    error,
+    login,
+  };
+}
